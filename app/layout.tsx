24bb28f85e44e229b7fd37a34baceee4a785415c
lisaorg <amol.yadav.778@gmail.com>
2024-09-14 22:23:18 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import { Footer } from "@/components/footer";
 import "./globals.css";
-
+import { DATA } from "@/schema.config";
 import dynamic from "next/dynamic";
 
 const VersionContextProvider = dynamic(
@@ -14,10 +14,9 @@ const VersionContextProvider = dynamic(
 );
 
 export const metadata: Metadata = {
-  title: "AriaDocs - Template",
-  metadataBase: new URL("https://ariadocs.vercel.app/"),
-  description:
-    "This comprehensive documentation template, crafted with Next.js and available as open-source, delivers a sleek and responsive design, tailored to meet all your project documentation requirements.",
+  title: DATA.siteName,
+  metadataBase: new URL(DATA.siteUrl),
+  description: DATA.siteDescription,
 };
 
 export default function RootLayout({
